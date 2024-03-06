de45fedf5f0703e1972abd5d438442d6aaaaf93b
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-03-06 16:33:58 +0500

    fix svg icon

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -4,6 +4,8 @@ export default function HomePage() {
   const hoverStyling = "hover:tracking-wider duration-300";
   const svgPath = "src/assets/lines.svg";
   const fancyBtn = "bg-[#ffff] py-2 px-4 rounded-xl";
+  const imgPath =
+    "https://images.pexels.com/photos/1981468/pexels-photo-1981468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
 
   return (
     <>
@@ -12,7 +14,7 @@ export default function HomePage() {
           <Header />
           <div
             id="home-main"
-            className="py-10 md:py-20 lg:py-26 flex flex-grow flex-col items-center gap-4 md:gap-6 xl:gap-10 px-3"
+            className="py-10 md:py-20 lg:py-32 xl:py-24 flex flex-grow flex-col items-center gap-4 md:gap-6 xl:gap-10 px-3"
           >
             <div className="flex items-end justify-center w-full">
               <h1 className="text-7xl md:text-8xl serif drop-shadow-lg">
@@ -47,12 +49,12 @@ export default function HomePage() {
         </div>
 
         <div id="home-img" className="flex-grow text-left xl:w-2/12">
-          <span className="flex gap-1 items-center xl:absolute xl:right-10 xl:top-6 xl:text-[#F3EFEA]">
+          <span className="flex gap-1 items-center xl:absolute xl:right-10 xl:top-6 xl:text-[#F3EFEA] pl-3">
             <h2 className={`${hoverStyling}`}>Latest Collection</h2>
             <i className="fa-solid fa-arrow-right"></i>
           </span>
           <img
-            src="https://images.pexels.com/photos/1981468/pexels-photo-1981468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            src={imgPath}
             alt="Your Image"
             className="object-cover w-full h-full"
           />
